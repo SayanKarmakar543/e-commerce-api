fcaf1e01e24321eca54505c0aeacdb822cd0d64b
Sayan Karmakar <sayankarmakar543@gmail.com>
2025-08-09 18:11:03 +0530

    created all the CRUD logic for user endpoints.

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -2,22 +2,34 @@ from fastapi import APIRouter
 from app.db.session import db_dependency
 from app.schemas.user_schema import UserRequest, UserResponse
 from starlette import status
+import uuid
 from app.repositories.user_repository import (
     get_user_repository,
-    create_user_repository
+    create_user_repository,
+    update_user_repository,
+    delete_user_repository
 )
 
 router = APIRouter()
 
-@router.get("/users", status_code=status.HTTP_200_OK)
-def get_users(db: db_dependency):
+
+@router.get(
+    "/users", 
+    status_code=status.HTTP_200_OK
+)
+async def get_users(db: db_dependency):
     """
     Retrieve all users from the database.
     """
     return get_user_repository(db)
 
-@router.post("/users", status_code=status.HTTP_201_CREATED,response_model=UserResponse)
-def create_user(
+
+@router.post(
+    "/users", 
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserResponse
+)
+async def create_user(
     db: db_dependency, 
     user_request: UserRequest,
 ):
@@ -25,3 +37,35 @@ def create_user(
     Create a new user in the database.
     """
     return create_user_repository(db, user_request)
+
+
+@router.put(
+    "/users/{id}", 
+    status_code=status.HTTP_200_OK, 
+    response_model=UserResponse
+)
+async def update_user(
+    db: db_dependency,
+    id: uuid.UUID,
+    user_request: UserRequest
+):
+    """
+    Update an existing user in the database.
+    """
+    return update_user_repository(db, id, user_request)
+
+
+@router.delete(
+    "/user/{id}",
+    status_code=status.HTTP_200_OK
+)
+async def delete_user(
+    db: db_dependency,
+    id: uuid.UUID
+):
+    
+    """
+    Delete a user from the database.
+    """
+    
+    return delete_user_repository(db, id)
--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.schemas.user_schema import UserRequest
 from fastapi import HTTPException
 from fastapi import status
+import uuid
 
 
 def get_user_repository(db: Session):
@@ -14,6 +15,9 @@ def get_user_repository(db: Session):
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Users not found."
         )
+
+    return users
+
 
 def create_user_repository(db: Session, user_request: UserRequest):
 
@@ -46,3 +50,59 @@ def create_user_repository(db: Session, user_request: UserRequest):
         "status": create_user_model.is_active,  # map to 'status'
         "created_at": create_user_model.created_at,
     }
+
+
+def update_user_repository(db: Session, user_id: uuid, user_request: UserRequest):
+
+    user_model = db.query(User).filter(User.id==user_id).first()
+
+    if not user_model:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found."
+        )
+    
+    # Check if the email already exists for another user
+    existing_email = db.query(User).filter(
+        User.email==user_request.email, 
+        User.id!=user_id
+    ).first()
+
+    if existing_email:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already exists."
+        )
+    
+    user_model.name = user_request.name
+    user_model.email = user_request.email
+    user_model.password_hash = user_request.password
+    user_model.role = user_request.role
+    user_model.is_active = user_request.status
+    user_model.created_at = user_request.created_at
+
+    db.add(user_model)
+    db.commit()
+    
+    return {
+        "id": user_model.id,
+        "name": user_model.name,
+        "email": user_model.email,
+        "role": user_model.role,
+        "status": user_model.is_active,
+        "created_at": user_model.created_at,
+    }
+
+
+def delete_user_repository(db: Session, user_id: uuid):
+
+    user_model = db.query(User).filter(User.id==user_id).first()
+
+    if not user_model:
+        raise HTTPException(
+            status=status.HTTP_404_NOT_FOUND,
+            detail="User not found."
+        )
+
+    db.delete(user_model)
+    db.commit()
